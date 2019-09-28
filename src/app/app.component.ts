c2bab28b430c854fe75f8806e351cb955126eae6
Ian Carpenter <ianhcarpenter@gmail.com>
2019-09-28 15:12:58 +0100

    Initial Version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HackerNewsService } from './shared/hackernews.service';
+import { AvailableStories } from './shared/enums.model';
+import { log } from 'util';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'ichn';
+
+export class AppComponent implements OnInit {
+
+  newStories: any[];
+  topStories: any[];
+  bestStories: any[];
+
+  constructor(private hnService: HackerNewsService) {}
+
+  ngOnInit(): void {
+    this.getStories(AvailableStories.new);
+    this.getStories(AvailableStories.top);
+    this.getStories(AvailableStories.best);
+  }
+
+  getStories(whichStory: number) {
+    this.hnService.getStories(whichStory).subscribe((stories) => {
+      console.log(stories);
+      switch (whichStory) {
+        case AvailableStories.new:
+          this.newStories = stories;
+          break;
+        case AvailableStories.top:
+          this.topStories = stories;
+          break;
+        case AvailableStories.best:
+          this.bestStories = stories;
+          break;
+      }
+    });
+  }
+
+  // return a string with number hour(s) | minute(s) ago
+  // 8 hours ago or 1 minute ago
+  calculateTimePosted(timePosted: number): string {
+
+    const timePostedAsDate = new Date(timePosted * 1000).getTime();
+
+    const now = new Date().getTime();
+
+    const timeDifference = Math.floor(now - timePostedAsDate) / 1000;
+
+    // pass to formatting
+
+    const days = Math.floor(timeDifference / 86400);
+    const hours = Math.floor(timeDifference / 3600) % 24;
+    const minutes = Math.floor(timeDifference / 60) % 60;
+    // not interested in seconds
+    // const seconds = Math.floor(timeDifference % 60);
+
+    if (days > 0) {
+      if (days === 1) {
+        return days + ' day ago';
+      } else if (days > 1) {
+        return days + ' days ago';
+      }
+    } else if (hours > 0) {
+      if (hours === 1) {
+        return hours + ' hour ago';
+      } else if (hours > 1) {
+        return hours + ' hours ago';
+      }
+    } else if (minutes === 1) {
+        return minutes + ' minute ago';
+      } else if (minutes !== 1) {
+        return minutes + ' minutes ago';
+      }
+    }
 }
+
---
+++ src/app/shared/hackernews.service.ts
@@ -0,0 +1,64 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, forkJoin } from 'rxjs';
+import { mergeMap } from 'rxjs/operators';
+import { AvailableStories } from './enums.model';
+
+@Injectable({ providedIn: 'root' })
+
+export class HackerNewsService {
+
+    private readonly newStoriesURL: string = 'https://hacker-news.firebaseio.com/v0/newstories.json';
+    private readonly topStoriesURL: string = 'https://hacker-news.firebaseio.com/v0/topstories.json';
+    private readonly bestStoriesURL: string = 'https://hacker-news.firebaseio.com/v0/beststories.json';
+
+    constructor(private http: HttpClient) { }
+
+    // marshalling method - obtains the ids for the story and then passes them on to the next
+    // observable to obtain the details for the id's supplied
+    getStories(storyRequested: number) {
+
+        const storyURL = this.getURL(storyRequested);
+
+        return this.getIdsForStories(storyURL).pipe(
+            mergeMap((ids) => forkJoin(ids.map((id) => this.getStoryDetails(id)))),
+        );
+    }
+
+    getURL(storyRequested: number): string {
+
+        let storyRequestedURL: string;
+
+        switch (storyRequested) {
+
+            case AvailableStories.new:
+                storyRequestedURL = this.newStoriesURL;
+                break;
+
+            case AvailableStories.top:
+                storyRequestedURL = this.topStoriesURL;
+                break;
+
+            case AvailableStories.best:
+                storyRequestedURL = this.bestStoriesURL;
+                break;
+        }
+        return storyRequestedURL;
+    }
+
+    // returns an array of ids for the story
+    getIdsForStories(url: string): Observable<any> {
+        return this.http.get(url, {
+            params: {
+                orderBy: '"$key"',
+                limitToFirst: '10',
+            }
+        });
+    }
+
+    // returns JSON containing the details of the post
+    getStoryDetails(id: number) {
+        // note the backtick used instead of quotes because of the ${id} parameter
+        return this.http.get(`https://hacker-news.firebaseio.com/v0/item/${id}.json`);
+    }
+}
